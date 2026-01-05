4aff8b736ff1684228e6e1108f8444c210d396f4
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2026-01-05 13:32:35 +0000

    Disable hash checking in tests (only compare with reference image)

--- tests/common.py
+++ tests/common.py
@@ -7,31 +7,10 @@ from domdf_python_tools.paths import PathPlus
 
 if sys.version_info[:2] == (3, 7):
 	baseline_dir = str(PathPlus(__file__).parent / "baseline_37")
-	image_hashes = str(PathPlus(__file__).parent / "image_hashes_37.json")
-elif sys.version_info[:2] == (3, 8):
-	baseline_dir = str(PathPlus(__file__).parent / "baseline")
-	image_hashes = str(PathPlus(__file__).parent / "image_hashes_38.json")
-elif sys.version_info[:2] == (3, 9):
-	baseline_dir = str(PathPlus(__file__).parent / "baseline")
-	image_hashes = str(PathPlus(__file__).parent / "image_hashes_39.json")
-elif sys.version_info[:2] == (3, 10):
-	baseline_dir = str(PathPlus(__file__).parent / "baseline")
-	image_hashes = str(PathPlus(__file__).parent / "image_hashes_313.json")
-elif sys.version_info[:2] == (3, 11):
-	baseline_dir = str(PathPlus(__file__).parent / "baseline")
-	image_hashes = str(PathPlus(__file__).parent / "image_hashes_313.json")
-elif sys.version_info[:2] == (3, 12):
-	baseline_dir = str(PathPlus(__file__).parent / "baseline")
-	image_hashes = str(PathPlus(__file__).parent / "image_hashes_313.json")
-elif sys.version_info[:2] == (3, 13):
-	baseline_dir = str(PathPlus(__file__).parent / "baseline")
-	image_hashes = str(PathPlus(__file__).parent / "image_hashes_313.json")
 else:
 	baseline_dir = str(PathPlus(__file__).parent / "baseline")
-	image_hashes = str(PathPlus(__file__).parent / "image_hashes.json")
 
 check_images = pytest.mark.mpl_image_compare(
 		baseline_dir=baseline_dir,
 		savefig_kwargs={"dpi": 600},
-		hash_library=image_hashes,
 		)
